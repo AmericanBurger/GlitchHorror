60556c127df27fea3a1ee8e977fda84a8ee04f0b
Sebastian Krajewski <215779@edu.p.lodz.pl>
2019-04-03 00:47:17 +0200

    Added whole project, which is 1 room xD

---
+++ Assets/Scripts/Drzwi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drzwi : MonoBehaviour {
+    public bool otwarte = false;
+    public float speed = 10f;
+
+    public Vector3 openAngles = new Vector3(0, 0, 0);
+    private Vector3 closed = Vector3.zero;
+    private Vector3 current = Vector3.zero;
+
+    private void Start()
+    {
+        closed = transform.eulerAngles;
+        current = closed;
+    }
+
+    private void Update()
+    {
+        if (otwarte)
+        {
+            current = Vector3.Lerp(current, openAngles, Time.deltaTime * speed);
+        } else
+        {
+            current = Vector3.Lerp(current, closed, Time.deltaTime * speed);
+        }
+        transform.rotation = Quaternion.Euler(current);
+    }
+}
---
+++ Assets/Scripts/OtwarcieDrzwi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtwarcieDrzwi : MonoBehaviour {
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Drzwi")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Drzwi temp = other.gameObject.GetComponent<Drzwi>();
+                temp.otwarte = !temp.otwarte;
+            }
+        }
+    }
+}
